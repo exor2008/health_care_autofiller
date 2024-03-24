a0d74811f999130aa4d4058e0d23c325886cc675
exor2008 <exorsteam2008@gmail.com>
2024-03-23 21:05:49 -0400

    Wait for internet to start

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import os
 
 from dotenv import load_dotenv
@@ -7,4 +8,5 @@ from health_care_autofiller.bot import start_app
 load_dotenv()
 
 if __name__ == "__main__":
+    time.sleep(10) # wait to initialize network after reboot
     start_app()
